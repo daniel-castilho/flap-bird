1c2c9e33e7f8dd6a5a83d708b0c6a434a342f0f7
Daniel Castilho <dan.castilho@gmail.com>
2020-12-18 22:08:45 -0500

    Colisão com o chão + som de hit

--- jogo.js
+++ jogo.js
@@ -1,5 +1,8 @@
 console.log('[Daniel Castilho] Flappy Bird');
 
+const somDeHIT = new Audio();
+somDeHIT.src = './efeitos/hit.wav';
+
 const sprites = new Image();
 sprites.src ='./sprites.png';
 
@@ -63,29 +66,53 @@ const chao = {
     },
 };
 
-const flappyBird = {
-    sourceX: 0,
-    sourceY: 0,
-    largura: 34,
-    altura: 24,
-    x: 10,
-    y: 50,
-    gravidade: 0.24,
-    velocidade:0,
-    atualiza() {
-        flappyBird.velocidade += flappyBird.gravidade;
-        flappyBird.y = flappyBird.y + flappyBird.velocidade;
-    },
-    desenha() {
-        contexto.drawImage(
-            sprites,
-            flappyBird.sourceX, flappyBird.sourceY, // Sprite X, Sprite Y
-            flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
-            flappyBird.x, flappyBird.y,
-            flappyBird.largura, flappyBird.altura
-        );
-    },
-};
+function fazColisao(flappyBird, chao) {
+    const flappyBirdY = flappyBird.y + flappyBird.altura;
+    const chaoY = chao.y;
+
+    if (flappyBirdY >= chaoY) {
+        return true;
+    }
+    return false;
+}
+
+function criaFlappyBird() {
+    const flappyBird = {
+        sourceX: 0,
+        sourceY: 0,
+        largura: 34,
+        altura: 24,
+        x: 10,
+        y: 50,
+        pulo: 4.6,
+        pula() {
+            flappyBird.velocidade = - flappyBird.pulo;
+        },
+        gravidade: 0.24,
+        velocidade:0,
+        atualiza() {
+            if (fazColisao(flappyBird, chao)) {
+                somDeHIT.play();
+                setTimeout(() => {
+                    mudaParaTela(Telas.INICIO);
+                }, 600);
+                return;
+            }
+            flappyBird.velocidade += flappyBird.gravidade;
+            flappyBird.y = flappyBird.y + flappyBird.velocidade;
+        },
+        desenha() {
+            contexto.drawImage(
+                sprites,
+                flappyBird.sourceX, flappyBird.sourceY, // Sprite X, Sprite Y
+                flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
+                flappyBird.x, flappyBird.y,
+                flappyBird.largura, flappyBird.altura
+            );
+        },
+    };
+    return flappyBird;
+}
 
 // mensagemGetReady
 const mensagemGetReady = {
@@ -108,17 +135,27 @@ const mensagemGetReady = {
 
 //
 // Telas
+//
+const globais = {};
 let telaAtiva = {};
 function mudaParaTela(novaTela) {
     telaAtiva = novaTela;
+
+    if (telaAtiva.inicializa) {
+        telaAtiva.inicializa();
+    }
 }
 
 const Telas = {
     INICIO: {
+        inicializa() {
+            globais.flappyBird = criaFlappyBird();
+
+        },
         desenha() {
             planoDeFundo.desenha();
             chao.desenha();
-            flappyBird.desenha();
+            globais.flappyBird.desenha();
             mensagemGetReady.desenha();
         },
         click() {
@@ -134,10 +171,13 @@ Telas.JOGO = {
     desenha() {
         planoDeFundo.desenha();
         chao.desenha();
-        flappyBird.desenha();
+        globais.flappyBird.desenha();
+    },
+    click() {
+        globais.flappyBird.pula();
     },
     atualiza() {
-        flappyBird.atualiza();
+        globais.flappyBird.atualiza();
     }
 };
 
